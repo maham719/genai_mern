2211fd93d89138635fcd30498b0f9dc69f2a8a1d
syeda maham amjad <zohameer96@gmail.com>
2026-07-06 17:30:55 +0500

    updated cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 app.use(cors({
    origin: [
       "http://localhost:5173",
-      "https://genai-mern-frontend.vercel.app/",
+      "https://genai-mern-frontend.vercel.app",
     ],
     credentials:true
 }))
